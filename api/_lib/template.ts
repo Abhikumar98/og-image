a6a4295adff19d3e4951792dd777177a7c67511e
abhikumar98 <abhishekkumar35962@gmail.com>
2021-08-28 08:46:35 +0530

    fix: husky fix

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -123,7 +123,7 @@ export function getHtml(parsedReq: ParsedRequest) {
             </div>
             <div class="spacer">
             <div class="heading">${emojify(
-                md ? marked(text) : sanitizeHtml(text)
+                md ? marked('yo ooasdofhas;dlfk jlkasdjf ') : sanitizeHtml('yo ooasdofhas;dlfk jlkasdjf ')
             )}
             </div>
         </div>
